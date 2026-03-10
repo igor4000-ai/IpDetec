f821a5de1379e9e5f035711d98f068bac716ef3b
Igor Gashenko <igor_4000@mail.ru>
2026-03-10 15:08:17 +0300

    Add config.py file

---
+++ config.py
@@ -0,0 +1,28 @@
+"""
+Конфигурационные настройки приложения.
+"""
+
+from typing import Dict, Any
+
+
+class Config:
+    """Конфигурационные настройки приложения."""
+
+    # API endpoints
+    URL_IP: str = 'https://api.ipify.org'
+    BASE_URL_CITY: str = 'https://ipinfo.io'
+    URL_YD: str = 'https://cloud-api.yandex.net'
+    LINK_DL_FILE: str = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+
+    # Файловые настройки
+    FILE_NAME: str = 'info.json'
+
+    # Настройки запросов
+    REQUEST_TIMEOUT: int = 10  # секунды
+
+    # Параметры для загрузки на Яндекс.Диск
+    UPLOAD_PARAMS: Dict[str, Any] = {
+        'path': FILE_NAME,
+        'fields': 'href',
+        'overwrite': True
+    }
