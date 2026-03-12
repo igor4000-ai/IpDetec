80a0fd7152be78c38b028173efa4e3c37c945288
Igor Gashenko <igor_4000@mail.ru>
2026-03-12 08:15:18 +0300

    Edit main.py, config.py, add requirements.txt

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
-
 from typing import Dict, Any
 
 
 class Config:
-    """Конфигурационные настройки приложения."""
+    """Класс с конфигурационными настройками приложения."""
 
     # API endpoints
-    URL_IP: str = 'https://api.ipify.org'
-    BASE_URL_CITY: str = 'https://ipinfo.io'
-    URL_YD: str = 'https://cloud-api.yandex.net'
-    LINK_DL_FILE: str = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    URL_IP: str = "https://api.ipify.org"
+    BASE_URL_CITY: str = "https://ipinfo.io"
+    URL_YD: str = "https://cloud-api.yandex.net"
+    LINK_DL_FILE: str = "https://cloud-api.yandex.net/v1/disk/resources/upload"
 
     # Файловые настройки
-    FILE_NAME: str = 'info.json'
+    FILE_NAME: str = "info.json"
 
     # Настройки запросов
-    REQUEST_TIMEOUT: int = 10  # секунды
+    REQUEST_TIMEOUT: int = 5  # секунды
 
     # Параметры для загрузки на Яндекс.Диск
     UPLOAD_PARAMS: Dict[str, Any] = {
-        'path': FILE_NAME,
-        'fields': 'href',
-        'overwrite': True
+        "path": FILE_NAME,
+        "fields": "href",
+        "overwrite": True,
     }
